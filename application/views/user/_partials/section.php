883fbe654c3feda14fe11fa79aa9c38403ff2f31
dedymandiri <dedymandiri992@gmail.com>
2019-03-13 10:31:31 +0700

    tambah user

---
+++ application/views/user/_partials/section.php
@@ -0,0 +1,41 @@
+		<section class="home_banner_area">
+            <div class="banner_inner">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8">
+							<div class="banner_content">
+								<h2>Aplikasi Pemesanan Furniture <br />PT Jati Makmur Mandiri</h2>
+								<p>Merupakan sebuah perangkat lunak (software) yang digunakan untuk pemesanan produk furniture dengan berbasis website dan android.</p>
+								<a class="banner_btn" href="pesansekarang.html">Pesan Sekarang</a>
+								<a class="banner_btn" href="carapesan.html">Baca Cara Pesan</a>
+							</div>
+						</div>
+
+                        <!---------------- Bagian Buat kategori------------------>
+						<div class="col-lg-4">
+							<div class="home_right_box">
+								<a class="home_item" href="kategorisatu.html">
+								    <i class="flaticon-sofa"></i>
+								</a>
+								<a class="home_item" href="kategoridua.html">
+									<i class="flaticon-bed"></i>
+								</a>
+
+								<!--<div class="home_item">
+									<i class="flaticon-computer"></i>
+								</div>
+								<div class="home_item">
+									<i class="flaticon-mirror"></i>
+								</div>
+								<div class="home_item">
+									<i class="flaticon-closet"></i>
+								</div>
+								<div class="home_item">
+									<i class="flaticon-kitchen"></i>
+								</div>-->
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </section>
