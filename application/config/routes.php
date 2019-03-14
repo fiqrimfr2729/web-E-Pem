267a578d508b748afa223882ac8d93821a043d32
dedymandiri <dedymandiri992@gmail.com>
2019-03-14 09:40:18 +0700

    commit tambah home

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'overview';
-$route['admin'] = 'overview';
+$route['default_controller'] = 'Overview';
+$route['admin/Overview'];
+//$route['default_controller'] = 'home';
+//$route['user/home'];
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+?>
