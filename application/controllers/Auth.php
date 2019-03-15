1bcea84fa87dc3e52df34d4603a286305aa71c37
dealquishu <dwiandikaseptiandy1@gmail.com>
2019-03-15 19:40:13 +0700

    edited Login form

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,7 +12,7 @@ class Auth extends CI_Controller
     public function index()
     {
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required|');
+        $this->form_validation->set_rules('password', 'password', 'trim|required');
         if ($this->form_validation->run() == false) {
             $data['title'] = 'E-Pem Admin Panel';
             $this->load->view('templates/auth_header', $data);
@@ -30,7 +30,42 @@ class Auth extends CI_Controller
         $password = $this->input->post('password');
 
         $user = $this->db->get_where('user', ['email' => $email])->row_array();
-        var_dump($user);
-        die;
+
+        if ($user) {
+            //jika user aktif
+            //var_dump($user);
+            //die;
+
+            if (password_verify($password, $user['password'])) {
+                //password verify
+                $data = ['email' => $user['email']];
+                $this->session->set_userdata($data);
+                redirect('admin');
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password!</div>');
+                redirect('auth');
+            }
+
+            /* if ($user['is_active'] == 1) {
+                //cek password
+                
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This Email has not been Activacted!</div>');
+                redirect('auth');
+            }
+
+            */
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not Registered!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You Have been Logged out!</div>');
+        redirect('auth');
     }
 }
