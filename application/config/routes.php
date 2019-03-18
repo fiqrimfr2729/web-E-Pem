ff8ba834fd8b8a2d8f49221275bef284cd10f6be
dealquishu <dwiandikaseptiandy1@gmail.com>
2019-03-18 23:42:43 +0700

    login with session

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['default_controller'] = 'user';
+$route['default_controller'] = 'auth';
 
 
 $route['admin'] = 'admin/dashboard';
--- application/controllers/Admin/Dashboard.php
+++ application/controllers/Admin/Dashboard.php
@@ -6,10 +6,17 @@ class Dashboard extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if (isset($this->session->userdata['logged_in']))
+        {
+            $email = ($this->session->userdata['logged_in']['email']);
+        }else{
+            header("location: auth");
+        }
     }
 
     public function index()
     {
+        $data['user'] = $this->db->get_where('user',['email' => $this->session->userdata('email')])->row_array();
         $data['main_content'] = 'admin/dashboard';
         $data['title_dashboard'] = 'Dashboard';
         $this->load->view('admin/overview', $data);
