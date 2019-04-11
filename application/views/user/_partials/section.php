70407293573fcaa9007f4be1efb81767c03549f7
dedymandiri <dedymandiri992@gmail.com>
2019-04-11 21:40:18 +0700

    perubahan diadmin

--- application/views/user/_partials/section.php
+++ application/views/user/_partials/section.php
@@ -4,7 +4,7 @@
 					<div class="row">
 						<div class="col-lg-8">
 							<div class="banner_content">
-								<h2>Aplikasi<br/>Pemesanan Mebel & Kusen <br/>PT Jati Makmur Mandiri (JMM)</h2>
+								<h2><br><br>Pemesanan Mebel & Kusen <br/>PT Jati Makmur Mandiri (JMM)</h2>
 								<p>Merupakan sebuah perangkat lunak (software) yang digunakan untuk pemesanan produk Mebel dan kusen dengan berbasis website dan android.</p>
 								<a class="banner_btn" href="<?php echo base_url('user/pesansekarang') ?>">Pesan Sekarang</a>
 								<a class="banner_btn" href="<?php echo base_url('user/carapesan') ?>">Baca Cara Pesan</a>
@@ -14,13 +14,13 @@
                         <!---------------- Bagian Buat kategori------------------>
 						<div class="col-lg-4">
 							<div class="home_right_box">
-								<a style="color:white; " class="home_item" href="<?php echo base_url('user/furniture') ?>">
+								<a style="color:white; margin-button:10px; " class="home_item" href="<?php echo base_url('user/furniture') ?>">
 								    <img class="img-fluid" src="<?php echo base_url('img/furniture/sofa.png')?>"  alt="">
-									<br>MEBEL
+									<i>MEBEL</i>
 								</a>
 								<a style="color:white;" class="home_item" href="<?php echo base_url('user/kusen') ?>">
 									<img class="img-fluid" src="<?php echo base_url('img/furniture/meja.png')?>"  alt="">
-									<br>KUSEN
+									KUSEN
 								</a>
 
 								<!--<div class="home_item">
