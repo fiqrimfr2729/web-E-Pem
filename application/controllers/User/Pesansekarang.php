a2b175037a5130adefe0e01c7e702285730f7325
dealquishu <dwiandikaseptiandy1@gmail.com>
2019-05-14 08:51:35 +0700

    new edited dwi

--- application/controllers/User/Pesansekarang.php
+++ application/controllers/User/Pesansekarang.php
@@ -1,21 +1,44 @@
-<?php 
+<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 
- // controller di awali dengan huruf kapital
+// controller di awali dengan huruf kapital
 class Pesansekarang extends CI_Controller
 {
+
+    private $_table = "pesanan";
+
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('pesanan_model');
+        $this->load->helper('date');
     }
 
-    public function index(){
-        
-        
+    public function index()
+    {
+
+        $data['produk'] = $this->pesanan_model->getAll();
+        $data['kota'] = $this->pesanan_model->getByKota();
+        $data['profinsi'] = $this->pesanan_model->getByProvinsi();
+
         // user sebagai nama folder nya
         // contact itu untuk mamanggil method untuk urutan tampilan website / sbg Overview nya
-		$this->load->view('User/pesansekarang') ;
-	}
+        $this->load->view('User/pesansekarang', $data);
+    }
+
+    public function tambahPesan()
+    {
+        $this->input->post();
+        $this->id_pesanan = uniqid();
+        $this->nama_pemesan = $_POST['nama_pemesan'];
+        $this->kota = $_POST['kota'];
+        $this->tanggal = $_POST['tanggal'];
+        $this->kontak = $_POST['kontak'];
+        $this->alamat = $_POST['alamat'];
+        $this->produk = $_POST['produk'];
+        $this->db->insert($this->_table, $this);
+
+        redirect('User/pesansekarang');
+    }
 }
- 
--- application/models/Pesanan_model.php
+++ application/models/Pesanan_model.php
@@ -3,7 +3,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Pesanan_model extends CI_Model
 {
-    private $_table = 'pesanan';
+    private $_table = 'produk';
+    private $_tableKota = 'kota';
+    private $_tableProvinsi = 'provinsi';
+
 
     public $id_pesanan;
     public $nama_pemesan;
@@ -17,5 +20,15 @@ class Pesanan_model extends CI_Model
     public function getAll()
     {
         return $this->db->get($this->_table)->result();
+    }
+
+    public function getByKota()
+    {
+        return $this->db->get($this->_tableKota)->result();
+    }
+
+    public function getByProvinsi()
+    {
+        return $this->db->get($this->_tableProvinsi)->result();
     }
 }
